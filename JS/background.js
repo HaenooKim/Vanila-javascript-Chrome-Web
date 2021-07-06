315f20bf853acc145bd85f4764aab236adfce6a1
HaenooKim <68438969+HaenooKim@users.noreply.github.com>
2021-07-07 03:33:11 +0900

    add background image

---
+++ JS/background.js
@@ -0,0 +1,14 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpg", "3.jpeg", "4.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random()*images.length)];
+
+//Html에 새로운 Element를 생성
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+//appendChild : html에 추가하는 것
+document.body.appendChild(bgImage);
+
+
+console.log(bgImage);
